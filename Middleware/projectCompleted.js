28ab8b89ff8e29ad1afe0df2fde9fc360de0c0be
Daniel Martin <danielmartin1205@gmail.com>
2020-01-10 21:26:32 -0500

    added stretch for all info of projects

--- Middleware/projectCompleted.js
+++ Middleware/projectCompleted.js
@@ -1,5 +1,7 @@
 const db = require("../data/dbHelpers/projects");
 
+const taskDb = require("../data/dbHelpers/tasks");
+
 const convertCompletedAll = () => {
     return async(req, res, next) => {
         try {
@@ -18,7 +20,7 @@ const convertCompletedAll = () => {
                 next();
             }
         }catch(err) {
-            return res.status(500).json({message: "Something went wrong"});
+            return res.status(500).json({message: "Something went wrong"})
         };
     };
 };
@@ -28,10 +30,28 @@ const convertCompleted = () => {
         try {
             let data = await db.findById(req.params.id);
             
+            let tasks = await taskDb.getProjectTasks(req.params.id);
+            
+            let resources = await db.findProjectResoucres(req.params.id);
+
+            let mapTask = tasks.map(cur =>{
+                if (cur.completed > 1) {
+                    cur.completed = true;
+                } else {
+                    cur.completed = false;
+                };
+                return cur;
+            });
+
             data.completed ? data.completed = true : data.completed = false;
-            res.project = data;
+            res.project = {
+                ...data,
+                tasks,
+                resources
+            };
             next();
         } catch(err) {
+            console.log(err)
             return res.status(500).json({message: "Something went wrong"})
         };
     };
